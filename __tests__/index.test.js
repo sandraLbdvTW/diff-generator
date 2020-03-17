648771bd1c87f08ab715adb7bff69c891a82eba1
Sandra <sandramoroz@yandex.ru>
2020-03-17 21:37:27 +0300

    step 4

---
+++ __tests__/index.test.js
@@ -0,0 +1,15 @@
+import genDiff from '..';
+
+test("json gendiff", () => {
+  expect(genDiff('before.json', 'after.json')).toEqual(`{
+  host: hexlet.io
+- timeout: 50
++ timeout: 20
+- proxy: 123.234.53.22
+- follow: false
++ verbose: true
+}`)
+  expect(() => {genDiff('', '')}).toThrow();
+  expect(() => {genDiff('nonexistentFile', '')}).toThrow();
+  expect(() => {genDiff('before.json')}).toThrow();
+});
